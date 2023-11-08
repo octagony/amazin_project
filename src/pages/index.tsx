ee44cac279fc1bcf807c1f1e9debed3c21ca0ef0
octagony <moctagony@gmail.com>
2023-11-08 12:39:16 +0300

    [REFACTOR] Change font connection method

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,22 @@
 import Image from "next/image";
+import { Poppins } from "next/font/google";
 import Header from "@/components/Header/Header";
+import Hero from "@/components/Hero/Hero";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default function Home() {
   return (
-    <main>
-      <Header />
-    </main>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Hero />
+      </main>
+    </>
   );
 }
